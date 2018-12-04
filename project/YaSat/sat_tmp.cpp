a9bdd82032446128c6e494b487eecc4105ac3c15
jeff <kulugu2@gmail.com>
2018-12-04 20:23:13 +0800

    conflict driven learning v1

--- project/YaSat/sat_tmp.cpp
+++ project/YaSat/sat_tmp.cpp
@@ -23,12 +23,9 @@ public:
 };
 vector<vector<int> >clauses;
 vector<var> ans;
-vector<vector<int> > decision_vector;
 stack<stack<int> > decision_stack;
 int maxVarIndex;
 int clauses_size;
-int input_clauses_size;
-int return_level;
 int bcp(vector<vector<int> > &two_lit, vector<var> &var_list, int update, vector<int> &scoreboard) {
     if(update > 0) {
         var_list[update-1].val = 1;
@@ -132,202 +129,41 @@ int check_uni(vector<vector<int> > &two_lit, vector<var>& var_list) {
     }
     return -1;
 }
-bool abs_cmp(int i, int j) {
-    return (abs(i)<abs(j));
-}
-vector<int> resolve(vector<int> conflict_clause, int clause_num, int p) {
-    cout<<"resolve"<<endl;
-    cout<<clause_num<<endl;
-    vector<int> tmp(clauses[clause_num]);
-    sort(conflict_clause.begin(), conflict_clause.end(), abs_cmp);
-    sort(tmp.begin(), tmp.end(), abs_cmp);
 
-    vector<int> result;
-    vector<int>::iterator it1 = conflict_clause.begin();
-    vector<int>::iterator it2 = tmp.begin();
-    while(it1 != conflict_clause.end() && it2 != tmp.end()) {
-        if(abs(*it1) == p) {
-            it1++;
-            continue;
-        }
-        if(abs(*it2) == p) {
-            it2++;
-            continue;
-        }
-        if(abs(*it1) < abs(*it2)) {
-            result.push_back(*it1);
-            it1++;
-        }
-        else if(abs(*it1) > abs(*it2)){
-            result.push_back(*it2);
-            it2++;
-        }
-        else {
-            result.push_back(*it1);
-            it1++;
-            it2++;
-        }
-    }
-    while(it2 != tmp.end()) {
-        if(abs(*it2) != p)
-            result.push_back(*it2);
-        it2++;
-    }
-    while(it1 != conflict_clause.end()) {
-        if(abs(*it1) != p)
-            result.push_back(*it1);
-        it1++;
-    }
-    for(int i=0;i<result.size();i++) {
-        cout<<result[i]<<" ";
-    }
-    cout<<endl;
-    return result;
-    
-    
-}
-vector<int> conflict_learning(stack<int> decision, int clause_num) {
-    vector<int> dv;
-    cout<<"c1"<<endl;
-    while(!decision.empty()) {
-        dv.push_back(decision.top());
-        cout<<decision.top()<<" ";
-        decision.pop();
-    }
-    cout<<endl;
-    vector<int> conflict_clause(clauses[clause_num]);
-    while(1) {
-
-        int count = 0;
-        int most_recent = -1;
-        for(int i=0;i<dv.size();i++) {
-            for(int j=0;j<conflict_clause.size();j++) {
-                if(dv[i] == abs(conflict_clause[j])) {
-                    count++;
-                    if(most_recent == -1)
-                        most_recent = dv[i];
-                    break;
-                }
-            }
-        }
-        if(count <= 1)
-            break;
-        conflict_clause = resolve(conflict_clause, decision_vector[most_recent-1][1], most_recent);
-    }
-    for(int i=0;i<conflict_clause.size();i++) {
-        cout<<conflict_clause[i]<<" ";
-    }
-    cout<<endl;
-    return conflict_clause;
-
-}
-void add_clauses(vector<int> learned, vector<vector<int> > &two_lit, vector<var> &var_list, int watch_literal) {
-
-    int tmpi;
-    for(tmpi = input_clauses_size; tmpi < clauses_size; tmpi++) {
-        if(clauses[tmpi] == learned)
-            break;
-    }
-    if(tmpi == clauses_size) {
-        clauses.push_back(learned);
-        clauses_size++;
-        int two0, two1;
-        if(learned.size() == 1) {
-            two0 = 0;
-            two1 = -1;
-        }
-        else {
-            two0 = watch_literal;
-            if(two0 != 0)
-                two1 = 0;
-            else
-                two1 = 1;
-            if(clauses[clauses_size-1][two0] > 0) {
-                var_list[abs(clauses[clauses_size-1][two0])-1].pw.push_back(clauses_size-1);
-            }
-            else {
-                var_list[abs(clauses[clauses_size-1][two0])-1].nw.push_back(clauses_size-1);                
-            }
-            if(clauses[clauses_size-1][two1] > 0) {
-                var_list[abs(clauses[clauses_size-1][two1])-1].pw.push_back(clauses_size-1);
-            }
-            else {
-                var_list[abs(clauses[clauses_size-1][two1])-1].nw.push_back(clauses_size-1);                
-            }
-        }
-        vector<int> new_two;
-        new_two.push_back(two0);
-        new_two.push_back(two1);
-        two_lit.push_back(new_two);
-        
-    }
-    return;
-}
-int DPLL(vector<vector<int> > &two_lit, vector<var> &var_list, vector<int> scoreboard, int level) {
-
+int DPLL(vector<vector<int> > &two_lit, vector<var> &var_list, vector<int> scoreboard) {
     int t;
-    //stack<int> decision;
-label_bcp:
+    stack<int> decision;
     while((t = check_uni(two_lit, var_list)) != -1) {
         //cout<<"imply"<<t<<endl;
-        
+      
         if(var_list[abs(clauses[t][two_lit[t][0]])-1].val == 0){ //not assign
             decision_stack.top().push(abs(clauses[t][two_lit[t][0]]));
-            decision_vector[abs(clauses[t][two_lit[t][0]])-1][0] = level;
-            decision_vector[abs(clauses[t][two_lit[t][0]])-1][1] = t;
-
             bcp(two_lit, var_list, clauses[t][two_lit[t][0]], scoreboard);
-            cout<<"bcp "<<clauses[t][two_lit[t][0]]<<" "<<t<<" level "<<level<<endl;
         }
         else {
             decision_stack.top().push(abs(clauses[t][two_lit[t][1]]));
-            decision_vector[abs(clauses[t][two_lit[t][1]])-1][0] = level;
-            decision_vector[abs(clauses[t][two_lit[t][1]])-1][1] = t;
             bcp(two_lit, var_list, clauses[t][two_lit[t][1]], scoreboard);
-            cout<<"bcp "<<clauses[t][two_lit[t][1]]<<" "<<t<<" level "<<level<<endl;
         }
     }
     //   check sat
-    static int i;
-    static int flag_sat;
-    flag_sat = 1;
+    int i;
+    int flag_sat = 1;
     for(i=0;i<clauses_size; i++) {
-        if(two_lit[i][1] == -1) {
-            if(clauses[i][0]*var_list[abs(clauses[i][0])-1].val < 0)
-                return 0;
-            else
-                continue;
-        }
+        if(two_lit[i][1] == -1)
+            continue;
         int a = clauses[i][two_lit[i][0]];
         int b = clauses[i][two_lit[i][1]];
         int vala = var_list[abs(a)-1].val;
         int valb = var_list[abs(b)-1].val;
         //if(a * var_list[abs(a)-1].val <= 0 && b * var_list[abs(b)-1].val <= 0) {
-        if(a * vala <= 0 && b * valb <= 0) {    //check unsat
+        if(a * vala <= 0 && b * valb <= 0) {    
             flag_sat = 0;
             if(vala != 0 && valb != 0) {
-                cout<<"conflict "<<i<<endl;
-                vector<int> learned(conflict_learning(decision_stack.top(), i));
-                int watch_lit;
-                int second_big = 0;
-                for(int tmpi = 0;tmpi<learned.size();tmpi++) {
-                    if(decision_vector[abs(learned[tmpi])-1][0] == level)
-                        watch_lit = tmpi;
-                    else if(decision_vector[abs(learned[tmpi])-1][0] > second_big)
-                        second_big = decision_vector[abs(learned[tmpi])-1][0]; 
-                }
-                add_clauses(learned, two_lit, var_list, watch_lit);
-
-                cout<<"push"<<endl;
                 while(!decision_stack.top().empty()) {
                     var_list[decision_stack.top().top()-1].val = 0;
-                    cout<<"pop "<<decision_stack.top().top()<<endl;
                     decision_stack.top().pop();
                 }
                 decision_stack.pop();
-                return_level = second_big;
-                cout<<"return c"<<endl;
-                
                 return 0;
             }
         }
@@ -361,70 +197,34 @@ label_bcp:
         if(var_list[i].val == 0)
             break;
     }*/
-    static vector<int> dlis_score;
-    dlis_score = scoreboard;
+    vector<int> dlis_score(scoreboard);
     for(int i=0;i<maxVarIndex;i++) {
         if(var_list[i].val != 0) {
             dlis_score[i*2] = 0;
             dlis_score[i*2+1] = 0;
         }
     }
-    static vector<int>::iterator max_ele;
+    vector<int>::iterator max_ele;
     max_ele = max_element(dlis_score.begin(), dlis_score.end());
     int max_index = distance(dlis_score.begin(), max_ele);
     while(var_list[max_index/2].val != 0) {
         cout<<"error"<<endl;
     }
-    stack<int> decision;
     decision.push(max_index/2+1);
     decision_stack.push(decision);
-    cout<<"push "<<max_index/2+1<<endl;
-    decision_vector[max_index/2][0] = level+1;
-    decision_vector[max_index/2][1] = -1;
-    static int old_clauses_size; 
-    old_clauses_size = clauses_size;
-    static vector<int> new_scoreboard;
-    new_scoreboard = scoreboard;
+    vector<int> new_scoreboard(scoreboard);
     if(max_index % 2 == 0) {
         bcp(two_lit, var_list, (max_index/2+1), new_scoreboard);
-        cout<<"dpll "<< max_index/2+1<<endl;
+        //cout<<"dpll "<< max_index/2+1<<endl;
     }
     else {
         bcp(two_lit, var_list, -(max_index/2+1), new_scoreboard);
-        cout<<"dpll "<<-(max_index/2+1)<<endl;
+        //cout<<"dpll "<<-(max_index/2+1)<<endl;
     }
     
-    if(DPLL(two_lit, var_list, new_scoreboard, level+1))
+    if(DPLL(two_lit, var_list, new_scoreboard))
         return 1;
     else {
-        if(return_level != level) {
-            while(!decision_stack.top().empty()) {
-                var_list[decision_stack.top().top()-1].val = 0;
-                decision_stack.top().pop();
-            }
-            decision_stack.pop();
-            cout<<"return non_cho"<<endl;
-            return 0;
-        }
-        else {
-            if(clauses_size != old_clauses_size) {
-                for(int i=old_clauses_size;i<clauses_size;i++) {
-                    for(int j=0;j<clauses[i].size();j++) {
-                        if(clauses[i][j] > 0) {
-                            scoreboard[2*(abs(clauses[i][j])-1)]++;
-                        }
-                        else {
-                            scoreboard[2*(abs(clauses[i][j])-1)+1]++;
-                        }
-                    }
-                }
-                cout<<"goto"<<endl;
-                goto label_bcp;
-            }
-            cout<<"learn nothing"<<endl;
-        }
-        stack<int> decision;
-        decision.push(max_index/2+1);
         decision_stack.push(decision);
         new_scoreboard = scoreboard;
         if(max_index % 2 == 0) {
@@ -437,36 +237,15 @@ label_bcp:
         }
         //cout<<"d "<<(i+1)<<endl;
         
-        if(DPLL(two_lit, var_list, new_scoreboard, level+1)) {
+        if(DPLL(two_lit, var_list, new_scoreboard)) {
             return 1;
         } 
         else {
-            if(return_level != level) {
-                while(!decision_stack.top().empty()) {
-                    var_list[decision_stack.top().top()-1].val = 0;
-                    decision_stack.top().pop();
-                }
-                decision_stack.pop();
-                cout<<"return non_cho"<<endl;
-                return 0;
+            while(!decision_stack.top().empty()) {
+                var_list[decision_stack.top().top()-1].val = 0;
+                decision_stack.top().pop();
             }
-            else {
-                if(clauses_size != old_clauses_size) {
-                    for(int i=old_clauses_size;i<clauses_size;i++) {
-                        for(int j=0;j<clauses[i].size();j++) {
-                            if(clauses[i][j] > 0) {
-                                scoreboard[2*(abs(clauses[i][j])-1)]++;
-                            }
-                            else {
-                                scoreboard[2*(abs(clauses[i][j])-1)+1]++;
-                            }
-                        }
-                    }
-                    cout<<"goto"<<endl;
-                    goto label_bcp;
-                }
-            }
-            return_level = level-1;
+            decision_stack.pop();
             return 0;
         }
     }
@@ -481,17 +260,10 @@ int main(int argc, char** argv) {
     emptyvar.val = 0;
     vector<var> var_list(maxVarIndex, emptyvar);
     clauses_size = clauses.size();
-    input_clauses_size = clauses_size;
     vector<int> init_two(2, 0);
     vector<vector<int> > two_lit(clauses_size, init_two);
-    for(int i=0;i<maxVarIndex;i++) {
-        vector<int> tmp(2, 0);
-        decision_vector.push_back(tmp);
-    }
     vector<int> scoreboard(maxVarIndex*2, 0);
     
-    stack<int> init;
-    decision_stack.push(init);
     //int two_lit[clauses.size()][2];
     //vector<int[2]> tmp_two_lit(clauses.size(), {-1, -1});
     //two_lit = tmp_two_lit;
@@ -538,15 +310,13 @@ int main(int argc, char** argv) {
         if(two_lit[i][1] == -1) {   //  uni-clause
             //cout<<"uni "<<i<<endl;
             //cout<<"bcp "<<clauses[i][0]<<endl;
-            decision_stack.top().push(abs(clauses[i][two_lit[i][0]]));
-            decision_vector[abs(clauses[i][two_lit[i][0]])-1][0] = 0;
-            decision_vector[abs(clauses[i][two_lit[i][0]])-1][1] = i;
             bcp(two_lit, var_list, clauses[i][0], scoreboard);
         }
     }
     //cout<<"GO"<<endl;
-    cout<<clauses_size<<endl;
-    int sat = DPLL(two_lit, var_list, scoreboard, 0); 
+    stack<int> init;
+    decision_stack.push(init);
+    int sat = DPLL(two_lit, var_list, scoreboard); 
     if(sat) {
         cout<<"s SATISFIABLE"<<endl;
     }
@@ -564,7 +334,6 @@ int main(int argc, char** argv) {
         cout<<"0"<<endl;
     }
     
-    cout<<clauses_size<<endl;
     //cout<<"two lit"<<endl;
     /*
     for(int i=0;i<clauses_size;i++)
--- project/YaSat/test.cpp
+++ project/YaSat/test.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
 void func(int (*a)[2], int n) {
@@ -9,7 +9,19 @@ void func(int (*a)[2], int n) {
 }
 
 int main() {
-    int a[4][2] = { {0,1}, {2,3}, {4,5}, {6,7}};
-    func(&a[0] , 4);
+    //int a[4][2] = { {0,1}, {2,3}, {4,5}, {6,7}};
+    //func(&a[0] , 4);
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    vector<int> b;
+    b.push_back(1);
+    b.push_back(3);
+    b.push_back(3);
+    if(a==b)
+    cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
     return 0;
 }
